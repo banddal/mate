2b4ad259eeea5adfb69614f740cdf2edf3abda29
band17dal <band17dal@gmail.com>
2026-07-07 07:15:57 +0900

    Round 15: stabilize local dev cache

---
+++ scripts/clean-next-cache.mjs
@@ -0,0 +1,7 @@
+import { rm } from "node:fs/promises";
+import { join } from "node:path";
+
+const nextDir = join(process.cwd(), ".next");
+
+await rm(nextDir, { recursive: true, force: true });
+console.log("Removed .next cache");
